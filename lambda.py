4ada6f81bf6403b18350e972db4ee5dfeaf781e0
dlculver <60633859+dlculver@users.noreply.github.com>
2022-06-17 13:13:33 +0200

    added lambda.py

---
+++ lambda.py
@@ -0,0 +1,127 @@
+import math
+
+prime = 2
+
+class monomial:
+    ### defines monomials in Lambda algebra
+    def __init__(self, const: int, lambdastr):
+        self.constant = const
+        if isinstance(lambdastr, str):
+            temp = [int(x) for x in lambdastr[1:-1].split(',')]
+        else:
+            temp = lambdastr
+        self.lambdas = temp
+        ### constant refers to field scalar
+        ### lambdas refers to a word in lambda generators
+        ### just noticed that in our code, we haven't given the computer a way to handle strings like '(1,1,3)' since there is no constant term here. Need to add something which says that if the constant isn't present, then its 1.
+        ### need to write something saying that if the constant is 0, then we convert the monomial to zero where zero is (0,[]).
+
+    def __lt__(self, other):
+        for i in range(min(len(self.lambdas), len(other.lambdas))):
+            if self.lambdas[i] < other.lambdas[i]:
+                return True
+            if self.lambdas[i] >= other.lambdas[i]:
+                return False
+        return len(self.lambdas) < len(other.lambdas)
+
+    def isadmissible(self):
+        answer = True
+        n = len(self.lambdas)
+        for i in range(n-1):
+            if 2*self.lambdas[i] < self.lambdas[i+1]:
+                answer = False
+        return answer
+
+    def __mul__(self, other):
+        constant = self.constant * other.constant % prime
+        monom = self.lambdas + other.lambdas
+        return monomial(constant, monom)
+
+
+
+class polynomial:
+    ### defines polynomial expressions in Lambda algebra
+    def __init__(self, monoms: list):
+        self.lambdastuff = monoms.copy()
+
+    ## this method puts polynomials into lexicographic order
+    def orderit(self):
+        self.lambdastuff.sort()
+
+    ## This method reduces the polynomial in that it adds coefficients of the same monomial appears more than once and reduces the coefficients mod p
+    def reduce(self):
+        self.orderit()
+        lambda_instances  = self.lambdastuff
+        for i in range(len(lambda_instances)):
+            lambda_instances[i].constant = lambda_instances[i].constant % prime
+        i=0
+        while i < len(lambda_instances)-1:
+            j = i+1
+            while j < len(lambda_instances) and lambda_instances[i].lambdas == lambda_instances[j].lambdas:
+                lambda_instances[i].constant = (lambda_instances[i].constant + lambda_instances[j].constant) % prime
+                lambda_instances[j].constant = 0
+                j += 1
+            i = j ## starting where you stopped
+                #lambda_instances.pop(i+1)
+        new_lambda_instances = [monom for monom in lambda_instances if monom.constant != 0]
+        self.lambdastuff = new_lambda_instances
+        # for i in range(len(lambda_instances)):
+        #     if lambda_instances[i].constant == 0:
+        #         lambda_instances.pop(i)
+
+    ## This method defines addition for polynomials in the characteristic
+    def __add__(self, other):
+        monoms = self.lambdastuff + other.lambdastuff
+        poly = polynomial(monoms)
+        poly.reduce()
+        return poly
+
+    def __mul__(self, other):
+        L = []
+        for i in range(len(self.lambdastuff)):
+            for j in range(len(other.lambdastuff)):
+                L.append(self.lambdastuff[i]*other.lambdastuff[j])
+        return polynomial(L)
+
+def parsing(lambdastring: str) -> polynomial:
+    answer = []
+    monoms = lambdastring.split('+')
+    for mono in monoms:
+        if mono.split('(')[0] == '':
+            constant = 1
+        else:
+            constant = int(mono.split('(')[0])
+        answer.append(monomial(constant, '('+mono.split('(')[1]))
+    poly = polynomial(answer)
+    poly.reduce()
+    return poly
+
+def admissmono(mono:monomial)-> polynomial:
+        L = mono.lambdas
+        l = len(L)
+        if l <= 1:
+            return polynomial([mono])
+        elif mono.isadmissible():
+            return polynomial([mono])
+        else:
+            for k in range(l-1):
+                if 2*L[k] < L[k+1]:
+                    i = L[k]
+                    n = L[k+1]-2*L[k]-1
+                    temp = []
+                    M = monomial(mono.constant,L[:k])
+                    N = monomial(1,L[k+2:])
+                    for m in range(math.ceil(n-1/2)): # this for loop produces the lists
+                        temp += [M*monomial(math.comb(n-m-1,m)%prime, [i+n-m,2*i+1+m])*N]
+                    p = polynomial([])
+                    for r in range(len(temp)):
+                        p += admissmono(temp[r])
+                    return p
+
+def admisspoly(poly: polynomial) -> polynomial:
+    monos = poly.lambdastuff
+    admissiblemonos = [admissmono(mono) for mono in monos]
+    p = polynomial([])
+    for i in range(len(admissiblemonos)):
+        p += admissiblemonos[i]
+    return p
